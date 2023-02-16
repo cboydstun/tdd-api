bc6ad8720cc23e878f0d0110a3e40f0d0a370b2a
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-02-16 03:06:28 -0600

    diminished cors

--- app.js
+++ app.js
@@ -4,26 +4,7 @@ const app = express();
 
 // import and use CORS to allow cross-origin requests
 const cors = require("cors");
-app.use(cors("*"
-  // {origin: "http://localhost:3000", credentials: true, optionsSuccessStatus: 200, methods: "GET,HEAD,PUT,PATCH,POST,DELETE", preflightContinue: false},
-  // {origin: "http://localhost:3001", credentials: true, optionsSuccessStatus: 200, methods: "GET,HEAD,PUT,PATCH,POST,DELETE", preflightContinue: false},
-  // {origin: "http://34.235.15.185:8080/", credentials: true, optionsSuccessStatus: 200, methods: "GET,HEAD,PUT,PATCH,POST,DELETE", preflightContinue: false},
-  // {origin: "https://www.codingcurricula.com/", credentials: true, optionsSuccessStatus: 200, methods: "GET,HEAD,PUT,PATCH,POST,DELETE", preflightContinue: false},
-  // {origin: "https://www.codingcurricula.com", credentials: true, optionsSuccessStatus: 200, methods: "GET,HEAD,PUT,PATCH,POST,DELETE", preflightContinue: false},
-  // {origin: "https://codingcurricula.com/", credentials: true, optionsSuccessStatus: 200, methods: "GET,HEAD,PUT,PATCH,POST,DELETE", preflightContinue: false},
-  // {origin: "https://bounce-v1.vercel.app", credentials: true, optionsSuccessStatus: 200, methods: "GET,HEAD,PUT,PATCH,POST,DELETE", preflightContinue: false},
-  // {origin: "https://bounce-v1.vercel.app/", credentials: true, optionsSuccessStatus: 200, methods: "GET,HEAD,PUT,PATCH,POST,DELETE", preflightContinue: false},
-));
-
-const corsOptions = function(req, res, next){ 
-  res.header('Access-Control-Allow-Origin', '*'); 
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 
-  'Content-Type, Authorization, Content-Length, X-Requested-With');
-   next();
-}
-
-app.use(corsOptions);
+app.use(cors("*"));
 
 // import bodyparser middleware
 const bodyParser = require("body-parser");
