b668d746c244bde831a24baffbadb2f144a4ed53
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-11-25 14:16:28 -0600

    remove email and lead endpoints

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,7 @@ const { enhancedLoggingMiddleware } = require('../middlewares/security');
 
 //import routers
 const blogRouter = require('./blogRouter');
-const emailRouter = require('./emailRouter');
 const userRouter = require('./userRouter');
-const leadRouter = require('./leadRouter');
 const contactRouter = require('./contactRouter');
 const productRouter = require('./productRouter');
 
@@ -19,8 +17,6 @@ const apiV1Router = express.Router();
 //define routes
 apiV1Router.use('/users', userRouter);
 apiV1Router.use('/blogs', blogRouter);
-apiV1Router.use('/emails', emailRouter);
-apiV1Router.use('/leads', leadRouter);
 apiV1Router.use('/contacts', contactRouter);
 apiV1Router.use('/products', productRouter);
 
